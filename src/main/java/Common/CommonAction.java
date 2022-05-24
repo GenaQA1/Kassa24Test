6786e0650eb740cdf7cc31f83d98d6a0d2749eb9
admin <gena.chebotar01@mail.ru>
2022-05-24 20:57:37 +0300

    Change chromedriverLinux

--- src/main/java/Common/CommonAction.java
+++ src/main/java/Common/CommonAction.java
@@ -29,7 +29,7 @@ public class CommonAction {
         if(driver == null){
             switch (PLATFORM_AND_BROWSER){
                 case "win_chrome":
-                    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriverLinux");
+                    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
                     driver = new ChromeDriver();   //driver = new RemoteWebDriver(new URL("http://localhost:4445/wd/hub"), chromeOptions);
                     break;
                 default:
